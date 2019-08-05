0b3ef09d74bb5fc4ed9346d25adb855448180e4f
Oscar Söderlund <odsod@users.noreply.github.com>
2019-08-05 17:12:07 +0200

    Move ScanMessages to own file
    Preparation for removing MessageScanner.

--- messagescanner.go
+++ messagescanner.go
@@ -2,8 +2,6 @@ package xsens
 
 import (
 	"bufio"
-	"bytes"
-	"encoding/binary"
 	"io"
 )
 
@@ -33,44 +31,4 @@ func (m *MessageScanner) Message() Message {
 // Err returns the error encountered while scanning, if any.
 func (m *MessageScanner) Err() error {
 	return m.sc.Err()
-}
-
-func ScanMessages(data []byte, atEOF bool) (advance int, token []byte, err error) {
-	if len(data) == 0 && atEOF {
-		return 0, nil, nil
-	}
-	// scan to next message header index
-	indexOfMessage := bytes.Index(data, []byte{valueOfPreamble, valueOfBusIdentifier})
-	if indexOfMessage == -1 {
-		if data[len(data)-1] == valueOfPreamble {
-			// edge case: latest byte is a preamble, so keep it
-			return len(data) - 1, nil, nil
-		}
-		// found no message header, discard all data and read more
-		return len(data), nil, nil
-	}
-	message := data[indexOfMessage:]
-	if len(message) < indexOfLength+lengthOfLength {
-		// not enough data to read length: advance to message start and read more data
-		return indexOfMessage, nil, nil
-	}
-	indexOfData := indexOfData
-	lengthOfData := int(message[indexOfLength])
-	if lengthOfData == valueOfLengthExtended {
-		if len(message) < indexOfExtendedLength+lengthOfExtendedLength {
-			// not enough data to read extended length: advance to message start and read more data
-			return indexOfMessage, nil, nil
-		}
-		indexOfData = indexOfExtendedData
-		lengthOfData = int(binary.BigEndian.Uint16(
-			message[indexOfExtendedLength : indexOfExtendedLength+lengthOfExtendedLength]))
-	}
-	lengthOfMessage := indexOfData + lengthOfData + lengthOfChecksum
-	if len(message) < lengthOfMessage {
-		// not enough data to read data and checksum, advance to message start and read more data
-		return indexOfMessage, nil, nil
-	}
-	// message now contains the exact bytes of the next scanned message
-	message = message[:lengthOfMessage]
-	return indexOfMessage + lengthOfMessage, message, nil
 }
