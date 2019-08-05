2a38e117e778aa92c83462b5b3fc7540701c73d4
Oscar Söderlund <odsod@users.noreply.github.com>
2019-08-05 19:58:18 +0200

    Migrate CLI to use xsens.Client
    Preparation for deprecating the MeasurementDataScanner.

--- cmd/xsens/main.go
+++ cmd/xsens/main.go
@@ -8,21 +8,27 @@ import (
 	"io/ioutil"
 	"os"
 	"os/signal"
+	"strings"
+	"time"
 
 	"github.com/einride/xsens-go"
 	"github.com/einride/xsens-go/pkg/serial"
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
 	ctx := withCancelOnSignal(context.Background(), os.Interrupt)
 	flags := flag.NewFlagSet("xsens", flag.ExitOnError)
+	jsonFlag := flags.Bool("json", false, "use JSON output")
+	baudRateFlag := flags.Int("baudRate", int(serial.BaudRate115200), "baud rate for serial communication")
+	configTimeoutFlag := flags.Duration("configTimeout", time.Second, "timeout for config operations")
 	usage := func() {
 		fmt.Print(`
 usage:
 
-	xsens read <port>
-	xsens get-output-config <port> [-json]
-	xsens set-ouptut-config <port> <config.json>
+	xsens read [-baudRate <int>] <port>
+	xsens get-output-config [-baudRate <int>] [-json] [-configTimeout <duration>] <port>
+	xsens set-ouptut-config [-baudRate <int>] [-configTimeout <duration>] <port> <config.json>
 
 `)
 		flags.PrintDefaults()
@@ -40,123 +46,76 @@ usage:
 		}
 		return flags.Arg(i)
 	}
-	jsonFlag := flags.Bool("json", false, "use JSON output")
-	baudRateFlag := flags.Int("baudRate", int(serial.BaudRate115200), "baud rate for serial communication")
 	_ = flags.Parse(args)
+	portName := arg(0)
+	port, err := serial.Open(portName, serial.BaudRate(*baudRateFlag))
+	if err != nil {
+		fmt.Println(err)
+		usage()
+	}
+	if err := port.Flush(); err != nil {
+		panic(err)
+	}
+	client := xsens.NewClient(port)
+	g, ctx := errgroup.WithContext(ctx)
+	ctx, cancel := context.WithCancel(ctx)
+	g.Go(func() error {
+		<-ctx.Done()
+		return client.Close()
+	})
 	switch subcommand {
 	case "read":
-		if err := readMain(ctx, arg(0), serial.BaudRate(*baudRateFlag), *jsonFlag); err != nil {
-			fmt.Println(err)
-			usage()
-		}
+		g.Go(func() error {
+			defer cancel()
+			return readMain(ctx, client)
+		})
 	case "get-output-config":
-		if err := getOutputConfigMain(ctx, arg(0), serial.BaudRate(*baudRateFlag), *jsonFlag); err != nil {
-			fmt.Println(err)
-			usage()
-		}
+		g.Go(func() error {
+			defer cancel()
+			return getOutputConfigMain(ctx, client, *configTimeoutFlag, *jsonFlag)
+		})
 	case "set-output-config":
-		if err := setOutputConfigMain(ctx, arg(0), arg(1), serial.BaudRate(*baudRateFlag)); err != nil {
-			fmt.Println(err)
-			usage()
-		}
+		g.Go(func() error {
+			defer cancel()
+			return setOutputConfigMain(ctx, client, arg(1), *configTimeoutFlag)
+		})
 	default:
+		usage()
+	}
+	if err := g.Wait(); err != nil && !strings.Contains(err.Error(), "closed") {
+		fmt.Println(err)
 		usage()
 	}
 }
 
-func readMain(_ context.Context, portName string, baudRate serial.BaudRate, useJSON bool) (err error) {
-	port, err := serial.Open(portName, baudRate)
-	if err != nil {
+func readMain(ctx context.Context, client *xsens.Client) error {
+	if err := client.GoToMeasurement(ctx); err != nil {
 		return err
 	}
-	defer func() {
-		if errClose := port.Close(); errClose != nil {
-			err = errClose
+	for {
+		fmt.Println()
+		fmt.Println(client.MessageIdentifier())
+		for client.ScanMeasurementData() {
+			fmt.Printf("\t%v\n", client.DataType())
+			fmt.Printf("\t%+v\n", client.MeasurementData())
 		}
-	}()
-	if err := port.Flush(); err != nil {
-		return err
-	}
-	sc := xsens.NewMessageScanner(port)
-	// install interrupt handler
-	interruptChan := make(chan os.Signal, 1)
-	signal.Notify(interruptChan, os.Interrupt)
-	// go to measurement
-	if _, err := port.Write(xsens.NewMessage(xsens.MessageIdentifierGotoMeasurement, nil)); err != nil {
-		return err
-	}
-	var data xsens.MeasurementData
-loop:
-	for sc.Scan() {
-		select {
-		case <-interruptChan:
-			break loop
-		default:
-		}
-		msg := sc.Message()
-		if msg.Identifier() != xsens.MessageIdentifierMTData2 {
-			continue
-		}
-		if err := data.UnmarshalMTData2(msg); err != nil {
+		if err := client.Receive(ctx); err != nil {
+			if strings.Contains(err.Error(), "closed") {
+				return nil
+			}
 			return err
 		}
-		if useJSON {
-			js, err := json.Marshal(data)
-			if err != nil {
-				return err
-			}
-			fmt.Printf("%s\n", js)
-		} else {
-			text, err := data.MarshalText()
-			if err != nil {
-				return err
-			}
-			fmt.Printf("%s\n\n", text)
-		}
 	}
-	if err := sc.Err(); err != nil {
-		return err
-	}
-	return nil
 }
 
-func getOutputConfigMain(_ context.Context, portName string, baudRate serial.BaudRate, useJSON bool) (err error) {
-	port, err := serial.Open(portName, baudRate)
+func getOutputConfigMain(ctx context.Context, client *xsens.Client, timeout time.Duration, useJSON bool) error {
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+	if err := client.GoToConfig(ctx); err != nil {
+		return err
+	}
+	outputConfiguration, err := client.GetOutputConfiguration(ctx)
 	if err != nil {
-		return err
-	}
-	defer func() {
-		if errClose := port.Close(); errClose != nil && err == nil {
-			err = errClose
-		}
-	}()
-	if err := port.Flush(); err != nil {
-		return err
-	}
-	sc := xsens.NewMessageScanner(port)
-	// go to config
-	if _, err := port.Write(xsens.NewMessage(xsens.MessageIdentifierGotoConfig, nil)); err != nil {
-		return err
-	}
-	for sc.Scan() && sc.Message().Identifier() != xsens.MessageIdentifierGotoConfig.Ack() {
-		// scan for ack
-	}
-	if sc.Err() != nil {
-		return err
-	}
-	// request output configuration
-	if _, err := port.Write(xsens.NewMessage(xsens.MessageIdentifierReqOutputConfiguration, nil)); err != nil {
-		return err
-	}
-	for sc.Scan() && sc.Message().Identifier() != xsens.MessageIdentifierReqOutputConfiguration.Ack() {
-		// scan for ack
-	}
-	if sc.Err() != nil {
-		return err
-	}
-	// parse and print output configuration
-	var outputConfiguration xsens.OutputConfiguration
-	if err := outputConfiguration.Unmarshal(sc.Message().Data()); err != nil {
 		return err
 	}
 	if useJSON {
@@ -175,20 +134,9 @@ func getOutputConfigMain(_ context.Context, portName string, baudRate serial.Bau
 	return nil
 }
 
-func setOutputConfigMain(_ context.Context, portName string, jsonFile string, baudRate serial.BaudRate) (err error) {
-	port, err := serial.Open(portName, baudRate)
-	if err != nil {
-		return err
-	}
-	defer func() {
-		if errClose := port.Close(); errClose != nil && err == nil {
-			err = errClose
-		}
-	}()
-	if err := port.Flush(); err != nil {
-		return err
-	}
-	sc := xsens.NewMessageScanner(port)
+func setOutputConfigMain(ctx context.Context, client *xsens.Client, jsonFile string, timeout time.Duration) error {
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
 	// parse output configuration
 	js, err := ioutil.ReadFile(jsonFile)
 	if err != nil {
@@ -204,26 +152,11 @@ func setOutputConfigMain(_ context.Context, portName string, jsonFile string, ba
 		return err
 	}
 	fmt.Printf("Setting output configuration:\n\n%s\n", txt)
-	// go to config
-	if _, err := port.Write(xsens.NewMessage(xsens.MessageIdentifierGotoConfig, nil)); err != nil {
+	if err := client.GoToConfig(ctx); err != nil {
 		return err
 	}
-	for sc.Scan() && sc.Message().Identifier() != xsens.MessageIdentifierGotoConfig.Ack() {
-		// scan for ack
-	}
-	if sc.Err() != nil {
+	if err := client.SetOutputConfiguration(ctx, outputConfiguration); err != nil {
 		return err
-	}
-	// set output configuration
-	data, err := outputConfiguration.Marshal()
-	if err != nil {
-		return err
-	}
-	if _, err := port.Write(xsens.NewMessage(xsens.MessageIdentifierSetOutputConfiguration, data)); err != nil {
-		return err
-	}
-	for sc.Scan() && sc.Message().Identifier() != xsens.MessageIdentifierSetOutputConfiguration.Ack() {
-		// scan for ack
 	}
 	return nil
 }
