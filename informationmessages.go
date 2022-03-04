6c7d34691ed1cc6a8aa84394d79774f35fd1b07b
Tristan Nicholls <tvkn@users.noreply.github.com>
2022-03-04 16:30:10 +0100

    feat: add get for productCode and HWVersion

--- informationmessages.go
+++ informationmessages.go
@@ -7,6 +7,10 @@ import (
 
 type DeviceID uint32
 
+type ProductCode string
+
+type HWVersion string // MAJOR.minor
+
 func (d *DeviceID) UnmarshalBinary(data []byte) error {
 	const mti100 = 4
 	const mti600 = 8
@@ -18,5 +22,18 @@ func (d *DeviceID) UnmarshalBinary(data []byte) error {
 	default:
 		return fmt.Errorf("unexpected DeviceID length: want: (%d or %d), got: %d", mti100, mti600, l)
 	}
+	return nil
+}
+
+func (d *ProductCode) UnmarshalBinary(data []byte) error {
+	*d = ProductCode(data)
+	return nil
+}
+
+func (d *HWVersion) UnmarshalBinary(data []byte) error {
+	if l := len(data); l != 2 {
+		return fmt.Errorf("unexpected HWVersion length: want: %d, got: %d", 2, l)
+	}
+	*d = HWVersion(fmt.Sprintf("%d.%d", data[0], data[1]))
 	return nil
 }
